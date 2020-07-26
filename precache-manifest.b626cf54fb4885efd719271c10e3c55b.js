c7bcf579d3233f2f74623aaf848e088a3195e6f9
Gi-hyoen Jeong <airus2302@gmail.com>
2020-07-27 08:52:40 +0900

    Updates

---
+++ precache-manifest.b626cf54fb4885efd719271c10e3c55b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7969cf93a82e7ac25eaa33e0f0ddfb8d",
+    "url": "/reactjs_movie_app/index.html"
+  },
+  {
+    "revision": "8fa1a72e6d5f26ca77c0",
+    "url": "/reactjs_movie_app/static/css/main.02049bb2.chunk.css"
+  },
+  {
+    "revision": "697e48abbd7f0ba75f99",
+    "url": "/reactjs_movie_app/static/js/2.75f33243.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/reactjs_movie_app/static/js/2.75f33243.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8fa1a72e6d5f26ca77c0",
+    "url": "/reactjs_movie_app/static/js/main.dc0e8906.chunk.js"
+  },
+  {
+    "revision": "ea6fb5de7019a6ed157b",
+    "url": "/reactjs_movie_app/static/js/runtime-main.ff77f0af.js"
+  }
+]);
